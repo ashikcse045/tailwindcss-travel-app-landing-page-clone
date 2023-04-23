a55f8135df69afb4115a2d90cd81b34d340a15ed
ashik <ashikcse321@gmail.com>
2023-04-23 23:31:53 +0600

    travel app landing page clone with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container:{
+      center: true
+    },
+    extend: {
+      fontFamily:{
+        roboto: "'Roboto', sans-serif",
+        popins: "'Poppins', sans-serif",
+        fancy: "'Courgette', cursive"
+      },
+      colors:{
+        primary: '#FF4A52',
+        secondary: '#1EC6B6',
+        tertiary: '#040E27'
+      }
+    },
+  },
+  plugins: [],
+}
+
